e66d1c681c16a3e252eb64792f43909e5a5f196e
Karan Kumar <thekarankumar.dev@gmail.com>
2024-08-19 17:12:44 +0530

    nav-color-fixed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <section className='bg-[black] h-[10vh] flex justify-center items-center rounded-xl border border-[#c2c2c2]'>
+    <section className='bg-[#d8d8d8] h-[10vh] flex justify-center items-center rounded-xl border border-[#c2c2c2]'>
     
     <div className='text-xl font-medium text-white'>Sorting Visualizer</div>
     
